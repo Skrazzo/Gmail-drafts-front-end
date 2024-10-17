0da706e49d77b333e7caef87ab73f90eb295d95f
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-10-17 20:07:25 +0300

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import * as React from "react";
-import { Button } from "./components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Drafts from "@/components/Pages/Drafts";
 
 export default function App() {
     return (
-        <div>
-            <p>Heres a button</p>
-            <Button>Hello world</Button>
+        <div className="container mx-auto max-w-5xl">
+            <Tabs defaultValue="drafts">
+                <TabsList>
+                    <TabsTrigger value="mail">Mail</TabsTrigger>
+                    <TabsTrigger value="drafts">Drafts</TabsTrigger>
+                </TabsList>
+                <TabsContent value="mail"></TabsContent>
+                <TabsContent value="drafts">
+                    <Drafts />
+                </TabsContent>
+            </Tabs>
         </div>
     );
 }
