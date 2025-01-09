f0b4ccd792cb291c0e52fd9c8f511157bc9137fd
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2025-01-09 20:44:50 +0200

    asd

--- src/global.ts
+++ src/global.ts
@@ -1,11 +1,11 @@
 // Does not end with "/"
-export const API_URL: string = "http://localhost:9999";
-// export const API_URL: string = "https://mail.cuentosconvalor.es/api";
+// export const API_URL: string = "http://localhost:9999";
+export const API_URL: string = "https://mail.cuentosconvalor.es/api";
 
 export const errorMessage = (msg: string): string =>
-	`${msg} , please try again, or contact a developer if error persists.`;
+    `${msg} , please try again, or contact a developer if error persists.`;
 
 export const iconProps = {
-	size: 24,
-	strokeWidth: 1.25,
+    size: 24,
+    strokeWidth: 1.25,
 };
